3c8cc8907a9c168df4d7143a878d2ffc2de7bd7f
0Vipin0 <vipinmalik941@gmail.com>
2020-07-20 13:23:28 +0530

    Added array_question_test.dart of Common Element and duplicate element

--- lib/array/array_questions.dart
+++ lib/array/array_questions.dart
@@ -6,4 +6,59 @@ class ArrayQuestions {
     }
     return result;
   }
+
+  int nthSmallestElement(List<int> a, int index) {
+    a.sort();
+    return a[index - 1];
+  }
+
+  int nthLargestElement(List<int> a, int index) {
+    a.sort();
+    return a[a.length - index - 1];
+  }
+
+  List<int> commonElementUsingSort(List<int> a, List<int> b) {
+    a.sort();
+    b.sort();
+    int i = 0, j = 0;
+    final List<int> result = <int>[];
+    while (i < a.length && j < b.length) {
+      if (a[i] == b[j]) {
+        result.add(a[i]);
+        i++;
+        j++;
+      } else if (a[i] < b[j]) {
+        i++;
+      } else {
+        j++;
+      }
+    }
+    return result;
+  }
+
+  List<int> commonElementUsingSet(List<int> a, List<int> b) {
+    final int aLength = a.length;
+    final int bLength = b.length;
+    final Set<int> hashSet = aLength < bLength ? a.toSet() : b.toSet();
+    final Set<int> commonElements = <int>{};
+    if (aLength < bLength) {
+      for (int i = 0; i < b.length; i++) {
+        if (hashSet.contains(b[i])) {
+          commonElements.add(b[i]);
+        }
+      }
+    } else {
+      for (int i = 0; i < a.length; i++) {
+        if (hashSet.contains(a[i])) {
+          commonElements.add(a[i]);
+        }
+      }
+    }
+    return commonElements.toList();
+  }
+
+  List<int> removeDuplicates(List<int> a) {
+    final Set<int> tempSet = a.toSet();
+    return tempSet.toList();
+  }
 }
--- test/array_question_test.dart
+++ test/array_question_test.dart
@@ -4,10 +4,26 @@ import 'package:test/test.dart';
 void main() {
   final ArrayQuestions arrayQuestions = ArrayQuestions();
   final List<int> array = <int>[12, 2, 34, 45, 5, 6];
+  final List<int> array2 = <int>[2, 6, 25, 90, 67, 78];
+  final List<int> commonElement = <int>[2, 6];
   group("Array Questions", () {
     test("Reverse an Array", () {
       final List<int> reversedArray = array.reversed.toList();
       expect(arrayQuestions.reverse(array), reversedArray);
+    });
+    test("Common Elements between two array using Sort", () {
+      expect(
+          arrayQuestions.commonElementUsingSort(array, array2), commonElement);
+    });
+    test("Common Elements between two array using Set", () {
+      expect(
+          arrayQuestions.commonElementUsingSet(array, array2), commonElement);
+    });
+    test("Remove Duplicate Element", () {
+      final List<int> duplicateElement = <int>[2, 2, 4, 5, 6, 4, 7];
+      final List<int> noDuplicateElement = <int>[2, 4, 5, 6, 7];
+      expect(arrayQuestions.removeDuplicates(duplicateElement),
+          noDuplicateElement);
     });
   });
 }
